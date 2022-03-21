8b41b754617a76ddb74fe398165eef3be65d06ce
Tomas Vesco <tomas_vesco@hotmail.com>
2022-03-21 11:02:23 -0300

    MVC - Handlebars

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const Contenedor = require('./clase');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs',
+    defaultLayout: 'index.hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.get('/productos', function ( req, res ) {
+
+    p.getAll()
+    .then ( products => {
+        if(products == ''){
+            products.push({title: "No se encontraron datos"});
+        }
+        res.render('main', { products });
+    })
+});
+
+app.post('/productos', ( req, res ) => {
+    const { title, price, image } = req.body;
+
+    const newProduct = {
+        title: title,
+        price: price,
+        image: image
+    }
+
+    p.save(newProduct);
+
+    p.getAll()
+    .then( products => {
+        res.render('main', { products });
+    });
+});
+  
+
+app.listen(8080);
+
+const p = new Contenedor( './productos.txt' );
